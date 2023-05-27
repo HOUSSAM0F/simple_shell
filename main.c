0be37e0d918df7adef523b656e02121b5d235378
HOUSSAM0F <atiche.hossam7@gmail.com>
2023-05-27 02:25:25 +0100

    publish all

--- main.c
+++ main.c
@@ -1,27 +1,44 @@
 #include "shell.h"
-int main() {
-    char command[MAX_COMMAND_LENGTH];
-    pid_t pid;
-    int status;
-    while (1) {
-        prompt();
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
-            printf("\n");  // Handle end of file (Ctrl+D)
-            break;
-        }
-        size_t command_length = strlen(command);
-        if (command_length > 0 && command[command_length - 1] == '\n') {
-            command[command_length - 1] = '\0';  // Remove the newline character
-        }
-        if (_strcmp(command, "exit") == 0) {  // Check for "exit" command
-            break;  // Exit the shell
-        }
-        if (_strcmp(command, "") == 0) {  // Handle empty input
-            continue;
-        }
-        char *args[MAX_ARGUMENTS];
-        tokenize_command(command, args);
-        fork_exec(args[0], args);
-    }
-    return 0;
+
+/**
+* main - entry point
+* @ac: arg count
+* @av: arg vector
+*
+* Return: 0 on success, 1 on error
+*/
+int main(int ac, char **av)
+{
+info_t info[] = { INFO_INIT };
+int fd = 2;
+
+asm ("mov %1, %0\n\t"
+"add $3, %0"
+: "=r" (fd)
+: "r" (fd));
+
+if (ac == 2)
+{
+fd = open(av[1], O_RDONLY);
+if (fd == -1)
+{
+if (errno == EACCES)
+exit(126);
+if (errno == ENOENT)
+{
+_eputs(av[0]);
+_eputs(": 0: Can't open ");
+_eputs(av[1]);
+_eputchar('\n');
+_eputchar(BUF_FLUSH);
+exit(127);
+}
+return (EXIT_FAILURE);
+}
+info->readfd = fd;
+}
+populate_env_list(info);
+read_history(info);
+hsh(info, av);
+return (EXIT_SUCCESS);
 }
